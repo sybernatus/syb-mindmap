7bb52cee1a9e04f0748942f5e6d1a61a2c049bbb
Jérôme Garcia <jerome.garcia@bashroom.com>
2025-04-06 12:12:08 +0200

    feat: link bezier update

--- src/links/link.rs
+++ src/links/link.rs
@@ -5,30 +5,127 @@ use egui::epaint::CubicBezierShape;
 use egui::Rect;
 
 #[derive(Debug, Clone)]
+pub struct LinkStyleBezier {
+    pub offset: f32,
+    pub color: Color32,
+    pub width: f32,
+}
+
+impl Default for LinkStyleBezier {
+    fn default() -> Self {
+        Self { offset: 0.15, color: Color32::LIGHT_GRAY, width: 2.0 }
+    }
+}
+#[derive(Debug, Clone)]
 pub struct Link {
     pub node_source: Rect,
     pub node_target: Rect,
+    pub style: LinkStyleBezier,
 }
+
+
 
 impl Link {
 
     pub fn new_from_nodes(node_source: Rect, node_target: Rect) -> Self {
-        Self { node_source, node_target }
+        Self { node_source, node_target, style: LinkStyleBezier::default() }
     }
 
     pub fn draw_bezier(&self, ui: &egui::Ui) {
-        let points = vec![
+
+        let (p1, p2) = Self::compute_control_points_offset(
             self.node_source.center(),
-            Pos2::new(self.node_source.center().x + 50.0, self.node_source.center().y),
-            Pos2::new(self.node_source.center().x + 5.0, self.node_target.center().y),
-            self.node_target.center()
+            self.node_target.center(),
+            self.style.offset
+        );
+
+
+        let (
+            direction_x,
+            direction_y,
+            distance,
+            normal_x,
+            normal_y
+        ) = Self::get_link_metadata(self.node_source.center(), self.node_target.center());
+
+        println!("dist: {} - dx: {} - dy: {}",distance, direction_x / distance, direction_y / distance);
+
+        let p0x = match direction_x {
+            d if d > 0.0 => self.node_source.center().x + self.node_source.size().x / 2.0 - 10.0,
+            d if d < 0.0 => self.node_source.center().x - self.node_source.size().x / 2.0 + 10.0,
+            _ => 0.0
+        };
+        let p0y = match direction_y {
+            d if d > 0.0 => self.node_source.center().y + self.node_source.size().y / 2.0 - 5.0,
+            d if d < 0.0 => self.node_source.center().y - self.node_source.size().y / 2.0 + 5.0,
+            _ => 0.0
+        };
+        let p3x = match direction_x {
+            d if d > 0.0 => self.node_target.center().x - self.node_target.size().x / 2.0 + 10.0,
+            d if d < 0.0 => self.node_target.center().x + self.node_target.size().x / 2.0 - 10.0,
+            _ => 0.0
+        };
+
+        let p3y = match direction_y {
+            d if d > 0.0 => self.node_target.center().y - self.node_target.size().y / 2.0 + 5.0,
+            d if d < 0.0 => self.node_target.center().y + self.node_target.size().y / 2.0 - 5.0,
+            _ => 0.0
+        };
+
+        let p0 = Pos2::new(p0x, p0y);
+        let p3 = Pos2::new(p3x, p3y);
+
+        let points = [
+            p0,
+            p1,
+            p2,
+            p3
         ];
+
         let bezier = CubicBezierShape::from_points_stroke(
-            <[Pos2; 4]>::try_from(points).unwrap(),
+            points,
             false,
             Default::default(),
-            Stroke::new(1.5, Color32::LIGHT_GRAY)
+            Stroke::new(
+                self.style.width,
+                self.style.color
+            )
         );
         ui.painter().add(bezier);
+    }
+
+    fn get_link_metadata(
+        start: Pos2,
+        end: Pos2
+    ) -> (f32, f32, f32, f32, f32) {
+        let direction_x = end.x - start.x;
+        let direction_y = end.y - start.y;
+
+        let distance = (direction_x.powi(2) + direction_y.powi(2)).sqrt();
+
+        let normal_x = -direction_y / distance;
+        let normal_y = direction_x / distance;
+        ( direction_x, direction_y, distance, normal_x, normal_y)
+    }
+
+    fn compute_control_points_offset(
+        start: Pos2,
+        end: Pos2,
+        offset_ratio: f32,
+    ) -> (Pos2, Pos2) {
+
+        let (direction_x, direction_y, distance, normal_x, normal_y) = Self::get_link_metadata(start, end);
+
+        let control_point1 = Pos2 {
+            x: start.x + direction_x / 3.0 + normal_x * distance * offset_ratio,
+            y: start.y + direction_y / 3.0 + normal_y * distance * offset_ratio,
+        };
+
+        let control_point2 = Pos2 {
+            x: start.x + 2.0 * direction_x / 3.0 + normal_x * distance * -offset_ratio,
+            y: start.y + 2.0 * direction_y / 3.0 + normal_y * distance * -offset_ratio,
+        };
+
+        (control_point1, control_point2)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,18 @@ mod text_utils;
 mod nodes;
 mod links;
 
-use crate::nodes::node::Node;
-use eframe::egui;
-use eframe::emath::Pos2;
-use eframe::epaint::{Color32, Stroke};
-use egui::{CentralPanel, ViewportBuilder};
 use crate::links::link::Link;
+use crate::nodes::node::Node;
+use eframe::{egui, NativeOptions};
+use eframe::emath::Pos2;
+use egui::{CentralPanel, ViewportBuilder};
 
 fn main() {
     let options = eframe::NativeOptions {
         viewport: ViewportBuilder::default()
-            .with_position([40000.0, 100.0])
-            .with_inner_size([480.0, 300.0]),
-        ..Default::default()
+            .with_position([400.0, 100.0])
+            .with_inner_size([480.0, 900.0]),
+        ..NativeOptions::default()
     };
 
     eframe::run_simple_native("my egui app", options, move |ctx, _frame| {
@@ -25,19 +24,62 @@ fn main() {
                 "Hello, world!".to_string(),
                 Pos2::new(140.0, 120.0)
             );
-            let node_rec_1 = Node::get_rect(ctx, node_1.clone());
 
             let node_2 = Node::new_with_text(
                 "Hello, world 2!".to_string(),
-                Pos2::new(210.0, 210.0)
+                Pos2::new(360.0, 150.0)
             );
-            let node_rec_2 = Node::get_rect(ctx, node_2.clone());
 
+            let node_rec_1 = Node::get_rect(ctx, node_1.clone());
+            let node_rec_2 = Node::get_rect(ctx, node_2.clone());
             let link_1 = Link::new_from_nodes(node_rec_1, node_rec_2);
 
+
+            let node_3 = Node::new_with_text(
+                "Hello, world back!".to_string(),
+                Pos2::new(340.0, 250.0)
+            );
+
+            let node_4 = Node::new_with_text(
+                "Hello, world back back!".to_string(),
+                Pos2::new(160.0, 480.0)
+            );
+
+            let node_rec_3 = Node::get_rect(ctx, node_3.clone());
+            let node_rec_4 = Node::get_rect(ctx, node_4.clone());
+            let link_2 = Link::new_from_nodes(node_rec_3, node_rec_4);
+
+
+
+            let node_5 = Node::new_with_text(
+                "Hello, world back!".to_string(),
+                Pos2::new(80.0, 550.0)
+            );
+
+            let node_6 = Node::new_with_text(
+                "Hello, world back back!".to_string(),
+                Pos2::new(350.0, 680.0)
+            );
+
+            let node_rec_5 = Node::get_rect(ctx, node_5.clone());
+            let node_rec_6 = Node::get_rect(ctx, node_6.clone());
+            let link_2 = Link::new_from_nodes(node_rec_5, node_rec_6);
+            let link_3 = Link::new_from_nodes(node_rec_1, node_rec_6);
+            let link_4 = Link::new_from_nodes(node_rec_2, node_rec_5);
+
+
+            link_2.draw_bezier(ui);
+            link_3.draw_bezier(ui);
+            link_2.draw_bezier(ui);
+            link_4.draw_bezier(ui);
             link_1.draw_bezier(ui);
             node_1.draw(ui);
             node_2.draw(ui);
+            node_3.draw(ui);
+            node_4.draw(ui);
+            node_5.draw(ui);
+            node_6.draw(ui);
+
         });
     }).expect("TODO: panic message");
 }

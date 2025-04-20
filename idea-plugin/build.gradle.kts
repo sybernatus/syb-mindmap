e6fecec13ea0e202d47a72db40bcf4306b5ae6f9
Jérôme Garcia <jerome.garcia@bashroom.com>
2025-04-20 17:30:01 +0200

    chore: release tag '0.6.0'

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("0.5.0")
+    version.set("0.6.0")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -31,7 +31,7 @@ tasks {
     }
 
     patchPluginXml {
-        version.set("0.5.0")
+        version.set("0.6.0")
         sinceBuild.set("241")
         untilBuild.set("243.*")
     }

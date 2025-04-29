0f935163bffaf97eb12f25a197a425172dea93f6
Jérôme Garcia <jerome.garcia@bashroom.com>
2025-04-29 13:55:44 +0200

    feat(moon): replace cargo-make by moon

--- bash/repository/setup/index.bash
+++ bash/repository/setup/index.bash
@@ -7,7 +7,6 @@ trap 'echo "Error occurred at line $LINENO"' ERR
 
 mkdir --parents "${repoDir:?}/.bin"
 
-cargo install cargo-make --version 0.37.24 || true
 cargo install cargo-edit --version 0.13.2 || true
 sudo apt install inkscape || true
 curl --location "https://github.com/jgm/pandoc/releases/download/3.6.4/pandoc-3.6.4-linux-amd64.tar.gz" | tar -xz -C "${repoDir:?}/.bin"

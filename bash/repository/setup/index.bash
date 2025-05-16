ece33d3cadc3ccd3dc5f97d030ec51b7ab996e97
Jérôme Garcia <jerome.garcia@bashroom.com>
2025-05-16 14:02:29 +0200

    feat(github): add action for feature branches

--- bash/repository/setup/index.bash
+++ bash/repository/setup/index.bash
@@ -13,7 +13,7 @@ proto_version="0.49.1"
 # >>>> SCRIPT
 
 # Install inkscape
-sudo apt install inkscape || true
+sudo apt install inkscape git || true
 
 ## Install moon
 #curl --location "https://github.com/moonrepo/moon/releases/download/v${moon_version}/moon-x86_64-unknown-linux-gnu" --output "${repoDir:?}/.bin/moon"

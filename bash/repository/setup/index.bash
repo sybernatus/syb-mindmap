6ef91caf6447b5aebfc877376f9008c54114b06b
Jérôme Garcia <jerome.garcia@bashroom.com>
2025-05-16 16:03:28 +0200

    feat(github): add action for feature branches

--- bash/repository/setup/index.bash
+++ bash/repository/setup/index.bash
@@ -12,14 +12,6 @@ pandoc_version="3.6.4"
 proto_version="0.49.1"
 # >>>> SCRIPT
 
-# Install inkscape
-sudo apt install --yes git || true
-#sudo apt install --yes inkscape git || true
-
-## Install moon
-#curl --location "https://github.com/moonrepo/moon/releases/download/v${moon_version}/moon-x86_64-unknown-linux-gnu" --output "${repoDir:?}/.bin/moon"
-#chmod +x "${repoDir:?}/.bin/moon"
-
 # Install proto
 mkdir --parents "${repoDir:?}/.bin/proto-tmp"
 curl --location "https://github.com/moonrepo/proto/releases/download/v${proto_version}/proto_cli-x86_64-unknown-linux-gnu.tar.xz" | tar -xJf - --directory "${repoDir:?}/.bin/proto-tmp" --strip-components=1
@@ -29,11 +21,14 @@ chmod +x "${repoDir:?}/.bin/proto" "${repoDir:?}/.bin/proto-shim"
 
 "${repoDir:?}/.bin/proto" install
 
+# Install inkscape
+sudo apt install --yes inkscape || true
+
 # Install cargo-edit
-#cargo install cargo-edit --version "${cargo_edit_version}" || true
+cargo install cargo-edit --version "${cargo_edit_version}" || true
 
 # Install pandoc
-#curl --location "https://github.com/jgm/pandoc/releases/download/${pandoc_version}/pandoc-${pandoc_version}-linux-amd64.tar.gz" | tar -xz -C "${repoDir:?}/.bin"
-#mv "${repoDir:?}/.bin/pandoc-${pandoc_version}/bin/pandoc" "${repoDir:?}/.bin/pandoc"
-#rm --recursive --force "${repoDir:?}/.bin/pandoc-${pandoc_version}"
+curl --location "https://github.com/jgm/pandoc/releases/download/${pandoc_version}/pandoc-${pandoc_version}-linux-amd64.tar.gz" | tar -xz -C "${repoDir:?}/.bin"
+mv "${repoDir:?}/.bin/pandoc-${pandoc_version}/bin/pandoc" "${repoDir:?}/.bin/pandoc"
+rm --recursive --force "${repoDir:?}/.bin/pandoc-${pandoc_version}"
 # <<<< SCRIPT
